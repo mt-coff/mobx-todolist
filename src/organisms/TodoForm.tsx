aa930e23d890693598d8ceb58519155c486cfdbe
mt-coff <mt.coff.00@gmail.com>
2019-05-13 03:05:21 +0900

    delete unused module

--- src/organisms/TodoForm.tsx
+++ src/organisms/TodoForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { InputWithLabel } from '../molecules/InputWithLabel'
 import { TodoList as TodoListStore, Todo } from '../store'
 import { Button } from '../atoms/Button'
